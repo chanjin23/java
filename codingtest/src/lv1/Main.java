41c3ee036673fa9899054394d4c2c76736e06a44
Chanjin Lee <chanjin23@naver.com>
2024-02-19 21:37:15 +0900

    정수 내림차순으로 배치하기

---
+++ codingtest/src/lv1/Main.java
@@ -0,0 +1,22 @@
+package lv1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //정수 내림차순 배치하기
+        long n=493192L;
+        String[] list =String.valueOf(n).split("");
+        Arrays.sort(list, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder(); //객체를 append할때마다 새로 생성시키지 x
+        for (String s : list) {
+            sb.append(s);   //배열 합치기
+        }
+        Long answer = Long.parseLong(sb.toString());
+        System.out.println("answer = " + answer);
+
+    }
+}
--- codingtest/src/lv1/ex1.java
+++ codingtest/src/lv1/ex1.java
@@ -1,4 +1,4 @@
-package start;
+package lv1;
 
 public class ex1 {
     public static void main(String[] args) {
