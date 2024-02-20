9b1286d745d87f9ebb1cb647eb2bc416b1758e20
Chanjin Lee <chanjin23@naver.com>
2024-02-20 18:48:28 +0900

    하샤드 수

---
+++ codingtest/src/lv1/Main2.java
@@ -0,0 +1,28 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //하샤드 수
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        boolean answer = true;
+        //x의 자리 수 확인 및 합 더하기
+        int tmp =x;
+        int tmp2=0;
+        int sum=0;
+        while(tmp>0){
+            tmp2=tmp%10;
+            tmp/=10;
+            sum+=tmp2;
+        }
+        //x 나누기 sum
+        if(x%sum==0){
+            answer =true;
+        }else{
+            answer =false;
+        }
+        System.out.println("answer = " + answer);
+    }
+}
