e10b22336d4d687261074686821cf9d0f47444a6
Chanjin Lee <chanjin23@naver.com>
2024-02-23 01:34:50 +0900

    콜라츠의 추측

---
+++ codingtest/src/lv1/Collatz.java
@@ -0,0 +1,26 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class Collatz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int answer = 0;
+        long n =num;
+        while(n!=1){
+            if(n%2==0){
+                n/=2;
+            }
+            else{
+                n=n*3+1;
+            }
+            ++answer;
+
+            if(answer>=500){
+                System.out.println("-1");
+            }
+        }
+        System.out.println("answer = " + answer);
+    }
+}
