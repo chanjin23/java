07b6a9c682d24b9d9f92b5ca46a8096f7fd07fa1
Chanjin Lee <chanjin23@naver.com>
2024-02-22 12:14:08 +0900

    음양 더하기

---
+++ codingtest/src/lv1/SumOfPlusMinus.java
@@ -0,0 +1,20 @@
+package lv1;
+
+public class SumOfPlusMinus {
+    public static void main(String[] args) {
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true, false, true};
+        //값을 받아와서 signs가 양수면 + 음수면 -
+        int sum =0;
+        int len =absolutes.length;
+        for(int i=0;i<len;++i){
+            if(signs[i]){
+                sum+=absolutes[i];
+            }else{
+                sum-=absolutes[i];
+            }
+        }
+        int answer = sum;
+        System.out.println("answer = " + answer);
+    }
+}
