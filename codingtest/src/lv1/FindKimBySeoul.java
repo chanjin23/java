4ef59663eecf00430cbfc5198fdaf6fa608f1b88
Chanjin Lee <chanjin23@naver.com>
2024-02-24 23:41:04 +0900

    서울에서 김서방 찾기

---
+++ codingtest/src/lv1/FindKimBySeoul.java
@@ -0,0 +1,16 @@
+package lv1;
+
+public class FindKimBySeoul {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Lee", "Kim"};
+        String answer ="";
+        int len =seoul.length;
+        for(int i=0;i<len;++i){
+            if(seoul[i].equals("Kim")){
+                answer ="김서방은 " +i+"에 있다";
+                break;
+            }
+        }
+        System.out.println("answer = " + answer);
+    }
+}
