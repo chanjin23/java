d08c041cf493e022fb07b6e1f968c22d7fa03f1b
Chanjin Lee <chanjin23@naver.com>
2024-02-25 15:05:14 +0900

    핸드폰 번호 가리기

---
+++ codingtest/src/lv1/BlindPhoneNumber.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class BlindPhoneNumber {
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        String answer = "";
+        for(int i=0;i<phone_number.length();++i){
+            if(phone_number.length()-4>i){
+                answer +="*";
+            }
+        }
+        answer+=phone_number.substring(phone_number.length()-4,phone_number.length());
+        System.out.println("answer = " + answer);
+    }
+}
