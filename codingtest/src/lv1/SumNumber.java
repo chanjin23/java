35d01594f04aa8e89e3af3e177db3e3f00089bab
Chanjin Lee <chanjin23@naver.com>
2024-02-25 15:03:24 +0900

    없는 숫자 더하기

---
+++ codingtest/src/lv1/SumNumber.java
@@ -0,0 +1,19 @@
+package lv1;
+
+public class SumNumber {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+        int answer = -1;
+        //0~9까지 더하는거니까
+        int total =0;
+        for(int i=0;i<10;++i){
+            total+=i;
+        }
+        int totalNumbers=0;
+        for(int i=0;i<numbers.length;++i){
+            totalNumbers+=numbers[i];
+        }
+        answer=total-totalNumbers;
+        System.out.println("answer = " + answer);
+    }
+}
