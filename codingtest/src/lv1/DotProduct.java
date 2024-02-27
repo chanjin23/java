e2f05bf7fde23decd7c4c722a6f6f3227c0645c5
Chanjin Lee <chanjin23@naver.com>
2024-02-27 23:41:40 +0900

    내적

---
+++ codingtest/src/lv1/DotProduct.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class DotProduct {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4};
+        int[] b = {-3, -1, 0, 2};
+        int answer = 0;
+        //a,b랑 곱하고 더하기
+        int len =a.length;
+        for(int i=0;i<len;++i){
+            answer+=a[i]*b[i];
+        }
+        System.out.println("answer = " + answer);
+    }
+}
