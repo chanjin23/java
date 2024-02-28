aeccbdd761cd6d5cd26bdc0e360a4f23000d9f80
Chanjin Lee <chanjin23@naver.com>
2024-02-28 13:27:16 +0900

    수박수박수박수박수?

---
+++ codingtest/src/lv1/WaterMelon.java
@@ -0,0 +1,22 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class WaterMelon {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        StringBuilder answer = new StringBuilder();
+        //홀수면 수 짝수면 박
+        for(int i=0;i<n;++i){
+            if(i%2==0){
+                answer.append("수");
+            }else{
+                answer.append("박");
+            }
+
+        }
+
+        System.out.println(answer.toString());
+    }
+}
