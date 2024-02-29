f5158c1101be30db86f5f7d694114c8471cb4796
Chanjin Lee <chanjin23@naver.com>
2024-02-29 13:13:43 +0900

    가운데 글자 가져오기

---
+++ codingtest/src/lv1/GetMiddleLetter.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class GetMiddleLetter {
+    public static void main(String[] args) {
+        //가운데 글자 가져오기
+        String s = "abcde";
+        String answer = "";
+        //s가 짝수인경우 홀수인경우
+        int len =s.length();
+        if(len%2==1){
+            answer =s.substring(len/2,len/2+1);
+        }else{
+            answer =s.substring(len/2-1,len/2+1);
+        }
+        System.out.println("answer = " + answer);
+    }
+}
