79b7eca832bc82e8a111c98e1975af72dfdcd0e4
Chanjin Lee <chanjin23@naver.com>
2024-03-02 17:37:11 +0900

    문자열 다루기

---
+++ codingtest/src/lv1/UseString.java
@@ -0,0 +1,21 @@
+package lv1;
+
+public class UseString {
+    public static void main(String[] args) {
+        String s = "a234";
+        boolean answer = true;
+        //try-catch로 하면 되자나
+        if(s.length()==4||s.length()==6){
+            try{
+                int x = Integer.parseInt(s);
+            }catch(Exception e){
+                answer =false;
+                System.out.println("answer = " + answer);
+                System.exit(0);
+            }
+            System.out.println("answer = " + answer);
+            System.exit(0);
+        }
+        else System.exit(0);
+    }
+}
