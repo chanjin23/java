ca3bd9d334bca4e40bfe0a2a5034f7462dae0f70
Chanjin Lee <chanjin23@naver.com>
2024-03-13 15:34:32 +0900

    해결

--- codingtest/src/baekjoon/loop/Sol10952.java
+++ codingtest/src/baekjoon/loop/Sol10952.java
@@ -1,2 +1,26 @@
-package baekjoon.loop;public class Sol10952 {
+package baekjoon.loop;
+
+import java.util.*;
+import java.io.*;
+public class Sol10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int[] num = new int[2];
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            num[0] = Integer.parseInt(st.nextToken());
+            num[1] = Integer.parseInt(st.nextToken());
+            if (num[0] == 0 && num[1] == 0) {
+                break;
+            }
+            bw.write(String.valueOf(num[0] + num[1]));
+            bw.newLine();
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
 }
