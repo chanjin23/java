7c53af55974b776b5ed2500e488dbe83a7fe891f
Chanjin Lee <chanjin23@naver.com>
2024-03-13 14:42:28 +0900

    빠른 A+B 다시풀어보기

--- codingtest/src/baekjoon/loop/Sol15552.java
+++ codingtest/src/baekjoon/loop/Sol15552.java
@@ -1,2 +1,27 @@
-package baekjoon.loop;public class Sol15552 {
+package baekjoon.loop;
+
+import java.util.*;
+import java.io.*;
+public class Sol15552 {
+    public static void main(String[] args) throws IOException{
+        //빠른 A+B
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] input = new int[2];
+        for (int i = 0; i < n; ++i) {
+            st = new StringTokenizer(br.readLine()," ");
+            input[0] = Integer.parseInt(st.nextToken());
+            input[1] = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf((input[0] + input[1])));
+            bw.newLine();
+        }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
 }
