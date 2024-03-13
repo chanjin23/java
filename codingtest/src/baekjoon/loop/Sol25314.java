29ae9b974e555f313cdc442525d1a3d20349b9a0
Chanjin Lee <chanjin23@naver.com>
2024-03-13 14:41:15 +0900

    해결

--- codingtest/src/baekjoon/loop/Sol25314.java
+++ codingtest/src/baekjoon/loop/Sol25314.java
@@ -1,2 +1,20 @@
-package baekjoon.loop;public class Sol25314 {
+package baekjoon.loop;
+
+import java.io.*;
+import java.util.*;
+public class Sol25314 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = sc.nextInt();
+        for (int i = 1; i <= input; i += 4) {
+            br.write("long ");
+        }
+        br.flush();
+        br.write("int");
+        br.flush();
+
+        br.close();
+
+    }
 }
