ce4563136adf65b8287002fec6b433ed3a1e7517
Chanjin Lee <chanjin23@naver.com>
2024-03-16 12:14:22 +0900

    해결

--- codingtest/src/baekjoon/deep1/Sol10988.java
+++ codingtest/src/baekjoon/deep1/Sol10988.java
@@ -1,2 +1,19 @@
-package baekjoon.deep1;public class Sol10988 {
+package baekjoon.deep1;
+
+import java.util.*;
+public class Sol10988 {
+    public static void main(String[] args) {
+        //팰린드롬인지 확인하기
+        Scanner sc =new Scanner(System.in);
+        String input =sc.nextLine();
+        int len =input.length();
+        for(int i=0;i<len;++i){
+            if(input.charAt(i)==input.charAt(len-i-1)) continue;
+            else {
+                System.out.println("0");
+                return;
+            }
+        }
+        System.out.println("1");
+    }
 }
--- codingtest/src/baekjoon/deep1/Sol2444.java
+++ codingtest/src/baekjoon/deep1/Sol2444.java
@@ -1,2 +1,32 @@
-package baekjoon.deep1;public class Sol2444 {
+package baekjoon.deep1;
+
+import java.io.*;
+import java.util.*;
+public class Sol2444 {
+    public static void main(String[] args) throws IOException {
+        //별 찍기 -7
+        Scanner sc =new Scanner(System.in);
+        BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
+        int input =sc.nextInt();
+        for(int i=1;i<=input;++i){
+            for(int j=1;j<= input-i;++j){
+                bw.write(" ");
+            }
+            for(int k=1;k<=(2*i-1);++k){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        for(int i=1;i<=input-1;++i){
+            for(int k=1;k<=i;++k){
+                bw.write(" ");
+            }
+            for(int j=1;j<=(input*2-1)-2*i;++j){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
 }
