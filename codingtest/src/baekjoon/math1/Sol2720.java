2bc1cdad50fb5ad2be0d522210868c9812a1671c
Chanjin Lee <chanjin23@naver.com>
2024-03-20 14:58:30 +0900

    해결

--- codingtest/src/baekjoon/math1/Sol2720.java
+++ codingtest/src/baekjoon/math1/Sol2720.java
@@ -1,2 +1,42 @@
-package baekjoon.math1;public class Sol2720 {
+package baekjoon.math1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Sol2720 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+
+        for(int i=0;i<n;++i){
+            int price =Integer.parseInt(br.readLine());
+            int quarter = 0, dime = 0, nickel = 0, penny = 0;
+            while (price > 0) {
+                if (price - 25 >= 0) {
+                    price-=25;
+                    quarter++;
+                }else if(price-10>=0){
+                    price-=10;
+                    dime++;
+                }else if(price-5>=0){
+                    price-=5;
+                    nickel++;
+                }else if(price-1>=0){
+                    price-=1;
+                    penny++;
+                }else{
+                    break;
+                }
+            }
+            bw.write(String.valueOf(quarter) + " " + String.valueOf(dime) + " " + String.valueOf(nickel) + " " + String.valueOf(penny) + " ");
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
--- codingtest/src/baekjoon/math1/Sol2903.java
+++ codingtest/src/baekjoon/math1/Sol2903.java
@@ -1,2 +1,17 @@
-package baekjoon.math1;public class Sol2903 {
+package baekjoon.math1;
+
+import java.util.Scanner;
+
+public class Sol2903 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int input =sc.nextInt();
+        int prev=2;
+        int output =0;
+        for(int i=0;i<input;++i){
+            prev = (prev - 1) * 2 + 1;
+            output =prev;
+        }
+        System.out.println(output*output);
+    }
 }
