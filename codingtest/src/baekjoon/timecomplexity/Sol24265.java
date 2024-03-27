2eb060febe0a834a07faff57337a06bcb8d15415
Chanjin Lee <chanjin23@naver.com>
2024-03-28 02:42:48 +0900

    훑어보기

--- codingtest/src/baekjoon/timecomplexity/Sol24265.java
+++ codingtest/src/baekjoon/timecomplexity/Sol24265.java
@@ -1,2 +1,13 @@
-package baekjoon.timecomplexity;public class Sol24265 {
+package baekjoon.timecomplexity;
+
+import java.util.Scanner;
+
+public class Sol24265 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        long n=sc.nextInt();
+        System.out.println((n - 1) * n / 2);
+        System.out.println("2");
+
+    }
 }
