eefa53b1cdd9767d94b88c763c9ac1bb29a7bc80
Chanjin Lee <chanjin23@naver.com>
2024-03-29 16:04:49 +0900

    다시 풀어보기

--- codingtest/src/inflearn/introduction/array/Sol0208FindRank.java
+++ codingtest/src/inflearn/introduction/array/Sol0208FindRank.java
@@ -7,12 +7,22 @@ public class Sol0208FindRank {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
         StringTokenizer st=new StringTokenizer(br.readLine());
-        int[] array=new int[n];
-        int[] tmp=new int[n];
+        int[] rank =new int[n];
         for(int i=0;i<n;++i){
-            array[i] =Integer.parseInt(st.nextToken());
-            tmp[i] =Integer.parseInt(st.nextToken());
+            rank[i]=Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(array);
+        int[] answer =new int[n];
+        for(int i=0;i<n;++i){
+            int count =1;
+            for(int j=0;j<n;++j){
+                if(rank[j] >rank[i]){
+                    ++count;
+                }
+            }
+            answer[i]=count;
+        }
+        for(int i:answer){
+            System.out.print(i+" ");
+        }
     }
 }
