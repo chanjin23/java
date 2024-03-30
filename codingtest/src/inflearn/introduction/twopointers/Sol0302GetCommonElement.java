35d48a9f6a0212a51e0961a24d63bd11e1054542
Chanjin Lee <chanjin23@naver.com>
2024-03-30 14:24:33 +0900

    해결

--- codingtest/src/inflearn/introduction/twopointers/Sol0302GetCommonElement.java
+++ codingtest/src/inflearn/introduction/twopointers/Sol0302GetCommonElement.java
@@ -4,33 +4,30 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
+
 public class Sol0302GetCommonElement {
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
         StringTokenizer st=new StringTokenizer(br.readLine());
-        int[] arr1 = new int[n];
+        HashSet<Integer> set1=new HashSet<>();
         for(int i=0;i<n;++i){
-            arr1[i]=Integer.parseInt(st.nextToken());
+            set1.add(Integer.parseInt(st.nextToken()));
         }
         int m=Integer.parseInt(br.readLine());
         st=new StringTokenizer(br.readLine());
-        int[] arr2 = new int[m];
+        HashSet<Integer> set2=new HashSet<>();
         for(int i=0;i<m;++i){
-            arr2[i]=Integer.parseInt(st.nextToken());
+            set2.add(Integer.parseInt(st.nextToken()));
         }
-        int tmp=0;
-        int count =0;
-        int[] arr3 = new int[30001];
-        for(int i=0;i<n;++i){
-            tmp =arr1[i];
-            for(int j=0;j<m;++j){
-                if(tmp==arr2[j]){
-                    arr3[count]=tmp;
-                    count++;
-                }
-            }
+        set1.retainAll(set2);
+        ArrayList<Integer> list =new ArrayList<>(set1);
+        Collections.sort(list);
+        for (Integer i : list) {
+            System.out.print(i+" ");
         }
-        Arrays.sort(arr3);
+
+
     }
 }
