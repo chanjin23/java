e3ef043fb597a12ba540f6af26b34fad9eeab525
Chanjin Lee <chanjin23@naver.com>
2024-04-09 03:11:17 +0900

    보류

---
+++ codingtest/src/inflearn/introduction/stackqueue/Sol0508EmergencyRoom.java
@@ -0,0 +1,23 @@
+package inflearn.introduction.stackqueue;
+
+import java.util.*;
+import java.io.*;
+
+public class Sol0508EmergencyRoom {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int m=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        Queue<Integer> queue=new LinkedList<>();
+        int[] arr =new int[n];
+        for(int i=0;i<n;++i){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int i=0;
+        while(true){
+            int first=arr[i];
+
+        }
+    }
+}
