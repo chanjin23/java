5bd3ba865bd067f9ad329e7a26bc1ff8c88f0c76
Chanjin Lee <chanjin23@naver.com>
2024-04-10 00:00:00 +0900

    해결

--- codingtest/src/inflearn/introduction/stackqueue/Sol0507CurriculumDesign.java
+++ codingtest/src/inflearn/introduction/stackqueue/Sol0507CurriculumDesign.java
@@ -6,23 +6,20 @@ public class Sol0507CurriculumDesign {
         Scanner sc =new Scanner(System.in);
         String str1=sc.nextLine();
         String str2=sc.nextLine();
-        Queue<Character> queue1=new LinkedList<>();
-        Queue<Character> queue2=new LinkedList<>();
-
-        for(int i=0;i<str2.length();++i){
-           char ch =str2.charAt(i);
-           queue1.offer(ch);
+        Queue<Character> queue=new LinkedList<>();
+        for(int i=0;i<str1.length();++i){
+            queue.offer(str1.charAt(i));
         }
-        for(char ch:str1.toCharArray()){
-            queue2.offer(ch);
-        }
-        for(int i=0;i<str2.length();++i){
-            if(queue1.poll()==queue2.peek()){
-                queue2.poll();
+        for(char ch:str2.toCharArray()){
+            if(queue.contains(ch)){
+                if(queue.peek()==ch) queue.poll();
+                else {
+                    System.out.println("NO");
+                    return;
+                }
             }
         }
-        if(queue2.size()==0) System.out.println("YES");
+        if(queue.isEmpty()) System.out.println("YES");
         else System.out.println("NO");
-
     }
 }
