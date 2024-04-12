2f83cb90e2e65c3ea2bac17fb30f04ed798bd798
Chanjin Lee <chanjin23@naver.com>
2024-04-12 16:28:30 +0900

    다시 풀어보기(클래스활용)

--- codingtest/src/inflearn/introduction/stackqueue/Sol0508EmergencyRoom.java
+++ codingtest/src/inflearn/introduction/stackqueue/Sol0508EmergencyRoom.java
@@ -4,22 +4,44 @@ import java.util.*;
 import java.io.*;
 
 public class Sol0508EmergencyRoom {
+    public static class Patient{
+        int index=0;
+        int risk=0;
+        public Patient(int index, int risk){
+            this.index= index;
+            this.risk = risk;
+        }
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        int m=Integer.parseInt(br.readLine());
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        Queue<Integer> queue=new LinkedList<>();
-        int[] arr =new int[n];
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(br.readLine()," ");
+        int[] arr=new int[n];
+        Queue<Patient> queue=new LinkedList<>();
         for(int i=0;i<n;++i){
             arr[i]=Integer.parseInt(st.nextToken());
+            queue.offer(new Patient(i,arr[i]));
         }
-        int i=0;
-        while(true){
-            int first=arr[i];
-
-
-
+        int answer=0;
+        while(!queue.isEmpty()){
+            Patient tmp=queue.poll();
+            boolean flag=true;
+            for(Patient p: queue){
+                if(tmp.risk<p.risk){
+                    queue.offer(tmp);
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                answer++;
+                if(tmp.index==m){
+                    System.out.println(answer);
+                    return;
+                }
+            }
         }
     }
 }
