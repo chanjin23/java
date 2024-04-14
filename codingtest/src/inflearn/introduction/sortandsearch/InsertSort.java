0e2f3861e73c9471c2955ee72a004510a74a9737
Chanjin Lee <chanjin23@naver.com>
2024-04-14 17:09:02 +0900

    다시 풀어보기

---
+++ codingtest/src/inflearn/introduction/sortandsearch/InsertSort.java
@@ -0,0 +1,27 @@
+package inflearn.introduction.sortandsearch;
+
+import java.util.*;
+import java.io.*;
+public class InsertSort {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0;i<n;++i){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        for(int i=1;i<n;++i){
+            for(int j=i-1;j>=0;--j){
+                if(arr[j]> arr[j+1]){
+                    int tmp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+        }
+        for(int i=0;i<n;++i){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
