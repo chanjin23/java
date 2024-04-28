5db96256524477df9e6508f45b890a5e8075f0ab
Chanjin Lee <chanjin23@naver.com>
2024-04-28 18:07:02 +0900

    그래프 최단거리(BFS) Array방식

--- codingtest/src/inflearn/introduction/dfsbfs/Sol0714GraphShortestRouteBFSArray.java
+++ codingtest/src/inflearn/introduction/dfsbfs/Sol0714GraphShortestRouteBFSArray.java
@@ -1,6 +1,10 @@
 package inflearn.introduction.dfsbfs;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 //입력
 /*6 9
 1 4
@@ -12,17 +16,17 @@ import java.util.*;
 4 6
 6 2
 6 5*/
-public class Sol0714GraphShortestRouteBFS {
+public class Sol0714GraphShortestRouteBFSArray {
     static int n,m,level;
     static ArrayList<ArrayList<Integer>> graph;
     static int[] check;
-    static int[] answer;
+    static int[] distance;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt(); //node
         int m = sc.nextInt();   //line
         graph =new ArrayList<>();
-        answer=new int[n+1];
+        distance =new int[n+1];
         check=new int[n+1];
         for(int i=0;i<=n;++i){
             graph.add(new ArrayList<>());
@@ -33,10 +37,11 @@ public class Sol0714GraphShortestRouteBFS {
             graph.get(x).add(y);
         }
         check[1]=1;
+        distance[1]=0;
         bfs(1,0);
 
         for(int i=1;i<=n;++i){
-            System.out.println(i+": "+answer[i]);
+            System.out.println(i+": "+ distance[i]);
         }
     }
     public static void bfs(int v,int level){
@@ -46,11 +51,11 @@ public class Sol0714GraphShortestRouteBFS {
             int len =q.size();
             for(int i=0;i<len;++i){
                 int current=q.poll();
-                answer[current]=level;
                 for (int k:graph.get(current)) {
                     if (check[k] == 0) {
                         q.offer(k);
                         check[k] = 1;
+                        distance[k]=distance[current]+1;
                     }
                 }
             }
