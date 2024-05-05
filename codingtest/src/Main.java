c6b8f6f53296a82a381e2652a5c41d4c3fb016c2
Chanjin Lee <chanjin23@naver.com>
2024-05-05 23:15:50 +0900

    해시맵 해결

--- codingtest/src/Main.java
+++ codingtest/src/Main.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int k=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;++i){
-            arr[i]=sc.nextInt();
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int lt=0;
-        int rt=0;
-        int count =0;
-        int max=-1;
-        while(rt <n){
-            if(arr[rt]==0) count++;
-            while(count >k){
-                if(arr[lt]==0) count--;
-                lt++;
-            }
-            int tmp=rt-lt+1;
-            max=Math.max(max,tmp);
-            rt++;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<k;++i){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        System.out.println(max);
+        System.out.print(map.size() + " ");
+        for (int i = 0; i <= n - k-1; ++i) {
+            map.put(arr[i], map.get(arr[i]) - 1);
+            map.put(arr[i + k], map.getOrDefault(arr[i + k], 0) + 1);
+            if(map.get(arr[i])==0) map.remove(arr[i]);
+            System.out.print(map.size() + " ");
+        }
     }
 }
