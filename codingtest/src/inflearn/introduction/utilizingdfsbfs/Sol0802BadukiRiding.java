1a960f65f5bb981b54acbbd5209322298ba82472
Chanjin Lee <chanjin23@naver.com>
2024-05-05 23:39:57 +0900

    리팩토링

--- codingtest/src/inflearn/introduction/utilizingdfsbfs/Sol0802BadukiRiding.java
+++ codingtest/src/inflearn/introduction/utilizingdfsbfs/Sol0802BadukiRiding.java
@@ -3,7 +3,7 @@ package inflearn.introduction.utilizingdfsbfs;
 import java.util.Scanner;
 
 public class Sol0802BadukiRiding {
-    static int answer,c,n;
+    static int c,n;
     static int max=-1;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -13,19 +13,13 @@ public class Sol0802BadukiRiding {
         for(int i=0;i<n;++i) arr[i]=sc.nextInt();
 
         dfs(0, 0, arr);
-        System.out.println(answer);
+        System.out.println(max);
     }
 
     public static void dfs(int level, int sum, int[] arr) {
-        if (max < sum && sum <= c) {
-            max=sum;
-            answer=max;
-        }
+        if(sum >c) return;
         if(level==n){
-//            if (max < sum && sum <= c) {
-//                max=sum;
-//                answer=max;
-//            }
+            max = Math.max(max, sum);
         }else{
             dfs(level + 1, sum + arr[level], arr);
             dfs(level + 1, sum , arr);
