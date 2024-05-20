6dab976f09ab0686857cf74d886abee8347269fc
Chanjin Lee <chanjin23@naver.com>
2024-05-20 20:02:07 +0900

    해결

---
+++ codingtest/src/programmers/lv1/FoodFightCompetition.java
@@ -0,0 +1,22 @@
+package programmers.lv1;
+
+public class FoodFightCompetition {
+    public static void main(String[] args) {
+        //푸드 파이트 대회
+        int[] food = new int[]{1, 3, 4, 6};
+        int[] one=new int[food.length];
+        for(int i=0;i<food.length;++i){
+            one[i]=food[i]/2;
+        }
+        String answer = "";
+        for(int i=1;i<food.length;++i){
+            int tmp=one[i];
+            for(int j=0;j<tmp;++j){
+                answer+=i;
+            }
+        }
+        answer+="0";
+        answer+=(new StringBuilder(answer).reverse().toString().substring(1,answer.length()));
+        return answer;
+    }
+}
