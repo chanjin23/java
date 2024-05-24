0631f9cda41ed9764499833a8b3fca0ca1525eaa
Chanjin Lee <chanjin23@naver.com>
2024-05-25 00:55:30 +0900

    다이내믹 프로그래밍 원리

---
+++ codingtest/src/inflearn/introduction/dynamicprogramming/Sol1002CrossTheStoneBridge.java
@@ -0,0 +1,17 @@
+package inflearn.introduction.dynamicprogramming;
+
+import java.util.Scanner;
+
+public class Sol1002CrossTheStoneBridge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i <= n; ++i) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[n]);
+    }
+}
